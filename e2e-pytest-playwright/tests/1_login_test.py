3a0101bce82d2abba4a8b58aa22c750084caa8f5
Courtney Zhan <courtney@icloud.com>
2023-09-27 15:13:41 +1000

    uyp

--- e2e-pytest-playwright/tests/1_login_test.py
+++ e2e-pytest-playwright/tests/1_login_test.py
@@ -14,37 +14,27 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/../")
 from test_helper import TestHelper
 from pages.login_page import LoginPage
 
-
 class LoginTestCase(unittest.TestCase, TestHelper):
- 
  
   @classmethod
   def setUpClass(cls):
     print("setUpClass") 
-    
 
   @pytest.fixture(autouse=True)
   def setup(self, page: Page):
-    print("setup")
-    self.page = page
-    self.page.goto("https://travel.agileway.net")
+    self.driver = page
+    self.driver.goto(self.site_url())
         
   def test_sign_in_failed(self):
-    login_page = LoginPage(self.page)
+    login_page = LoginPage(self.driver)
     login_page.enter_username("agileway")
     login_page.enter_password("badpass")
     login_page.click_sign_in()
     assert "Invalid email or password" in self.page.content();
 
   def test_sign_in_ok(self):
-    # self.page.goto("https://travel.agileway.net")
-    login_page = LoginPage(self.page)
+    login_page = LoginPage(self.driver)
     login_page.enter_username("agileway")
     login_page.enter_password("testwise")
     login_page.click_sign_in()
     assert self.page.text_content("div#flash_notice") == "Signed in!"
-
-# if __name__ == '__main__':
-#     unittest.main(
-#         testRunner=xmlrunner.XMLTestRunner(output='reports'),
-#         failfast=False, buffer=False, catchbreak=False)
