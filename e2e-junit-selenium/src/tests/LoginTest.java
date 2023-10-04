b6ea9d1d0023b9fc891ddf23892ad0790d705e8d
Courtney Zhan <qinmengzhan@gmail.com>
2023-10-04 19:29:38 +1000

    test fixes

--- e2e-junit-selenium/src/tests/LoginTest.java
+++ e2e-junit-selenium/src/tests/LoginTest.java
@@ -28,7 +28,7 @@ public class LoginTest {
         loginPage.enterUsername("agileway");
         loginPage.enterPassword("guess");
         loginPage.clickSignIn();
-        Assert.assertEquals("Invalid email or password", driver.findElement(By.id("flash_alert1")).getText() ); 
+        Assert.assertEquals("Invalid email or password", driver.findElement(By.id("flash_alert")).getText() ); 
     }
 
     @Test
